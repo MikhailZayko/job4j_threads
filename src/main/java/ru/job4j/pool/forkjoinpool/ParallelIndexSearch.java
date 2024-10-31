d53145e1fb3bf01e66aea4c88b7fcd25132bbcae
Admin <mishavolgodonsk@gmail.com>
2024-10-31 21:25:07 +0300

    3. ForkJoinPool [#315067]

---
+++ src/main/java/ru/job4j/pool/forkjoinpool/ParallelIndexSearch.java
@@ -0,0 +1,53 @@
+package ru.job4j.pool.forkjoinpool;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+
+public class ParallelIndexSearch<T> extends RecursiveTask<Integer> {
+
+    private final T target;
+
+    private final T[] array;
+
+    private final int from;
+
+    private final int to;
+
+    public ParallelIndexSearch(T target, T[] array, int from, int to) {
+        this.target = target;
+        this.array = array;
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    protected Integer compute() {
+        if (to - from <= 10) {
+            return linearIndexSearch();
+        }
+        int middle = (from + to) / 2;
+        ParallelIndexSearch<T> leftSearch = new ParallelIndexSearch<>(target, array, from, middle);
+        ParallelIndexSearch<T> rightSearch = new ParallelIndexSearch<>(target, array, middle + 1, to);
+        leftSearch.fork();
+        rightSearch.fork();
+        Integer left = leftSearch.join();
+        Integer right = rightSearch.join();
+        return left != -1 ? left : right;
+    }
+
+    private Integer linearIndexSearch() {
+        int result = -1;
+        for (int i = 0; i < array.length; i++) {
+            if (target.equals(array[i])) {
+                result = i;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static <T> int search(T target, T[] array) {
+        return ForkJoinPool.commonPool()
+                .invoke(new ParallelIndexSearch<>(target, array, 0, array.length - 1));
+    }
+}
---
+++ src/test/java/ru/job4j/pool/forkjoinpool/ParallelIndexSearchTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.pool.forkjoinpool;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ParallelIndexSearchTest {
+
+    @Test
+    void whenTypeIsString() {
+        String[] array = new String[] {"One", "Two", "Three", "Four"};
+        int result = ParallelIndexSearch.search("Three", array);
+        assertThat(result).isEqualTo(2);
+    }
+
+    @Test
+    void whenTypeIsInteger() {
+        Integer[] array = new Integer[] {4, 6, 3, 9, 55, 4567};
+        int result = ParallelIndexSearch.search(55, array);
+        assertThat(result).isEqualTo(4);
+    }
+
+    @Test
+    void whenTheArraySizeIsLarge() {
+        Integer[] array = new Integer[40000];
+        IntStream.range(0, array.length).forEach(i -> array[i] = i);
+        int result = ParallelIndexSearch.search(33333, array);
+        assertThat(result).isEqualTo(33333);
+    }
+
+    @Test
+    void whenElementNotFound() {
+        Integer[] array = new Integer[4444];
+        IntStream.range(0, array.length).forEach(i -> array[i] = i);
+        int result = ParallelIndexSearch.search(5555, array);
+        assertThat(result).isEqualTo(-1);
+    }
+}
